c45ff18eb49b41baa2000d7d50767ec8ab2767eb
Marc-Daniel Ortega <globulon@gmail.com>
2012-04-09 19:18:01 +0200

    basic parser combinators ok

--- src/parsers/core.clj
+++ src/parsers/core.clj
@@ -1,6 +1,6 @@
 (ns parsers.core)
 
-(def zero  (fn [s] []))
+(def zero (fn [s] []))
 
 (def item
   (fn [input]
@@ -14,19 +14,17 @@
 (defn bind [parser-a f]
   (fn [input]
     (let [results (parser-a input)]
-     (vec (mapcat
-          (fn [[result remaining]] ((f result) remaining))
-          results)))))
+      (into (empty results) (mapcat
+             (fn [[result remaining]] ((f result) remaining))
+             results)))))
 
 (defn satisfy? [predicate]
-  (bind
-    item
-    (fn [input]
-      (if (predicate input)
-        (result input)
-        zero ))))
+  (bind item (fn [input]
+    (if (predicate input)
+      (result input)
+      zero))))
 
-(defn chr[c]
+(defn chr [c]
   (satisfy? (fn [input] (= c input))))
 
 (def digit (satisfy? (fn [c] (Character/isDigit c))))
@@ -45,10 +43,40 @@
 
 (def word
   (plus
-    (bind
-      letter
-      (fn [x]
-        (bind word
-          (fn [s]
-            (result (vec (cons x s)))))))
+    (bind letter (fn [x]
+      (bind word (fn [s]
+        (result (vec (cons x s)))))))
     (result "")))
+
+(defn many [parser]
+  (plus
+    (bind parser (fn [x]
+      (bind (many parser) (fn [xs]
+        (result (cons x xs))))))
+    (result "")))
+
+(defn to-digit [chr]
+  (Character/digit chr 10))
+
+(defn to-number [digits]
+  (reduce (fn [acc val] (+ (* 10 acc) (to-digit val))) 0 digits))
+
+(def natural
+  (bind
+    (many digit)
+    (fn [digits]
+      (if (empty? digits)
+        zero
+        (result (to-number digits))))))
+
+(def negative
+  (bind (chr \-)
+    (fn [_] (bind natural
+       (fn [y]
+        (if (number? y)
+          (result (* -1 y))
+          (result  y)))))))
+
+(def integer (plus natural negative))
+
+
---
+++ src/patterns/core.clj
@@ -0,0 +1,27 @@
+(ns patterns.core)
+
+(defprotocol Functor
+  (fmap [source f]))
+
+(defprotocol Monad
+  (return [source data])
+  (flat-map [source]))
+
+(extend-protocol Functor
+  clojure.lang.IPersistentCollection
+  (fmap [source f]
+    (into (empty source) (for [x source] (f x)))))
+
+(extend-protocol Monad
+  clojure.lang.IPersistentCollection
+
+  (return [source data]
+    (into (empty source) data))
+
+  (flat-map [source f]
+    (into (empty source)(mapcat f source))))
+
+
+;(defn bind [m f]
+;  (let [structure (fmap m f)
+;        join]))
